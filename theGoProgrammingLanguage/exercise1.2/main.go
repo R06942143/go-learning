c1d4157635f1f147ae1fcea1b5c9ee55cef68f6a
conradSHL <r06942143@ntu.edu.tw>
2023-05-08 22:46:36 +0800

    feat: exercise 1.2

---
+++ theGoProgrammingLanguage/exercise1.2/main.go
@@ -0,0 +1,13 @@
+// Modify the echo program to print the index and value of each of its arguments, one per line.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i := 0; i < len(os.Args); i++ {
+		fmt.Println(i, os.Args[i])
+	}
+}
