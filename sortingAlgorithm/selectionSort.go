4694229f9fdbbda5b170600d3310228afd2e50cb
conradSHL <r06942143@ntu.edu.tw>
2023-05-16 20:53:09 +0800

    feat: binary search

---
+++ sortingAlgorithm/selectionSort.go
@@ -0,0 +1,20 @@
+package main
+
+func binarySearch(array []int, target int) int {
+	low := 0
+	high := len(array) - 1
+	for low <= high {
+		mid := (low + high) / 2
+		switch {
+		case array[mid] == target:
+			return mid
+		case array[mid] > target:
+			high = mid - 1
+		case array[mid] < target:
+			low = mid + 1
+		}
+
+	}
+
+	return -1
+}
